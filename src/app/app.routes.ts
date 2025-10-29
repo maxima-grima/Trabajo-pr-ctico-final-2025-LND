7ca8ac329923789644107ade41f4a81fed8e3a8e
pilartm <ptorresmarelli@austral.edu.ar>
2025-10-29 11:07:10 -0300

    guard resgister

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { LoginPage } from './pages/login-page/login-page';
 import { onlyPublicUserGuard} from './guard/only-public-user-guard'; 
+import { RegisterPage } from './pages/register-page/register-page';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,11 @@ export const routes: Routes = [
     {
         path: "login",
         component: LoginPage,
+        canActivate: [onlyPublicUserGuard]
+    },
+    {
+        path: "register",
+        component: RegisterPage,
         canActivate: [onlyPublicUserGuard]
     },
 ];
