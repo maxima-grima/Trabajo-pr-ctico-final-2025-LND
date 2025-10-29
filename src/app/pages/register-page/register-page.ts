169b34fce6817edaa6ffb48ba71d7e9e592a92ee
maximagrima <mgrima@mail.austral.edu.ar>
2025-10-29 11:14:09 -0300

    29/10

--- src/app/pages/register-page/register-page.ts
+++ src/app/pages/register-page/register-page.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { UsersService } from '../../SERVICE/users-service';
+import { Spinner } from '../../Components/spinner/spinner';
+
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [RouterModule, FormsModule, Spinner],
   templateUrl: './register-page.html',
   styleUrl: './register-page.scss'
 })
-export class RegisterPage {
 
+
+export class RegisterPages {
+  errorRegister=false;
+  usersService = inject(UsersService);
+  isLoading = false;
+  router = inject(Router);
+
+  async register(form: NgForm){
+    this.errorRegister = false;
+    if(!form.value.email || 
+      !form.value.password || 
+      !form.value.password2 || 
+      !form.value.firstName ||
+      !form.value.lastName ||
+      form.value.password !== form.value.password2){
+      this.errorRegister = true;
+      return
+    }
+    this.isLoading = true;
+    const res = await this.usersService.register(form.value);
+    if(res.ok){
+      this.router.navigate(["/login"])
+    }
+    this.isLoading = false;
+    this.errorRegister = true;
+  }
 }
