3760fbb06333d4fb14f408eb95ca33dbe73c94c6
maximagrima <mgrima@mail.austral.edu.ar>
2025-11-05 09:02:18 -0300

    Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,23 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { LoginPage } from './pages/login-page/login-page';
+import { onlyPublicUserGuard} from './guard/only-public-user-guard'; 
 import { RegisterPage } from './pages/register-page/register-page';
+import { onlyLoggedUserGuard } from './guard/only-loged-user-guard';
+export const routes: Routes = [
+     {
+        path: "",
+        component: Home,
+    },
+    {
+        path: "login",
+        component: LoginPage,
+        canActivate: [onlyPublicUserGuard]
+    },
+    {
+        path: "register",
+        component: RegisterPage,
+        canActivate: [onlyPublicUserGuard]
+    },
 
-export const routes: Routes = [];
+];
