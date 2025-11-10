d5c2fdfb9793e4aad8e37e7269b3b32f32495eca
pilartm <ptorresmarelli@austral.edu.ar>
2025-11-10 17:48:33 -0300

    layout

--- src/app/layout/general-layout/general-layout.ts
+++ src/app/layout/general-layout/general-layout.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-general-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './general-layout.html',
   styleUrl: './general-layout.scss'
 })
